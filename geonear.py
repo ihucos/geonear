7bee6b45444d68b99bf479cda39039c545760697
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2014-11-10 17:12:15 +0100

    code cleanup (makes code more pep8)

--- geonear.py
+++ geonear.py
@@ -1,41 +1,40 @@
-#The MIT License (MIT)
+# The MIT License (MIT)
 #
-#Copyright (c) 2014 Irae Hueck Costa
+# Copyright (c) 2014 Irae Hueck Costa
 #
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 #
-# TODO: merge globe.data and globe.filter_data to take_data or something?
-# TODO: of course implement the "shake"
-# TODO: add namespace support, call it label?
-# TODO: find nicer method names, especially for almost_near and friends.
+#  TODO: merge globe.data and globe.filter_data to take_data or something?
+#  TODO: of course implement the "shake"
+#  TODO: add namespace support, call it label?
+#  TODO: find nicer method names, especially for almost_near and friends.
 
 import json
 import hashlib
 from random import choice
 
 import requests
-import geohash # install with `pip install python-geohash` and *not* `pip install geohash`
+import geohash  # install with `pip install python-geohash`
 
 
-
-PROJECT_URL = 'http://github.com/nomoral'
+PROJECT_URL = 'http://github.com/ihuecos/geonear'
 
 
 def hash_iter(args):
@@ -53,22 +52,25 @@ def hash_iter(args):
         master_hash.update(arg_hash)
     return master_hash.hexdigest()
 
-def geohash_and_neighbours(gh, neighbours_deepth=1):
+
+def geohash_and_neighbors(gh, neighbors_deepth=1):
     '''
-    >>> geohash_and_neighbours('bg4r', neighbours_deepth=0)
+    >>> geohash_and_neighbors('bg4r', neighbors_deepth=0)
     set(['bg4r'])
-    >>> sorted(geohash_and_neighbours('bg4r'))
+    >>> sorted(geohash_and_neighbors('bg4r'))
     ['bg4n', 'bg4p', 'bg4q', 'bg4r', 'bg4w', 'bg4x', 'bg60', 'bg62', 'bg68']
-    >>> sorted(geohash_and_neighbours('bg4r', neighbours_deepth=2))
+    >>> sorted(geohash_and_neighbors('bg4r', neighbors_deepth=2))
     ['bg1v', 'bg1y', 'bg1z', 'bg3b', 'bg3c', 'bg4j', 'bg4m', 'bg4n', 'bg4p', 'bg4q', 'bg4r', 'bg4t', 'bg4v', 'bg4w', 'bg4x', 'bg4y', 'bg4z', 'bg60', 'bg61', 'bg62', 'bg63', 'bg68', 'bg69', 'bg6b', 'bg6c']
 
     '''
-    # some neighbours are calculated many times, but for performance this makes almost no difference
+
+    # some neighbors are calculated repeatedly, that's ok for performance
     ghs = set([gh])
-    for i in range(neighbours_deepth):
-        for gh in tuple(ghs): # tuple because we want a copy from it
+    for i in range(neighbors_deepth):
+        for gh in tuple(ghs):  # tuple because we want to copy ghs
             ghs.update(geohash.expand(gh))
     return ghs
+
 
 def hscan(redis, *args, **kw):
     cursor = 0
@@ -76,37 +78,40 @@ def hscan(redis, *args, **kw):
         cursor, results = redis.hscan(*args, cursor=cursor, **kw)
         for key, val in results.items():
             yield key, val
-        if not int(cursor): # int because cursor is returned as string
+        if not int(cursor):  # int because cursor is returned as string
             break
+
 
 class NominatimGeocode(object):
     def __init__(self,
-            search_endpoint=None,
-            reverse_endpoint=None,
-            mail=None):
+                 search_endpoint=None,
+                 reverse_endpoint=None,
+                 mail=None):
         self._search_endpoint = (search_endpoint or
-                'http://nominatim.openstreetmap.org/search')
+                                 'http://nominatim.openstreetmap.org/search')
         self._reverse_endpoint = (reverse_endpoint or
-                'http://nominatim.openstreetmap.org/reverse')
+                                  'http://nominatim.openstreetmap.org/reverse')
         self._mail = mail or ''
-        self._headers = {'User-Agent': 'Globe 1.0 ({})'.format(PROJECT_URL)}
+        self._headers = {'User-Agent':
+                         'Geonear pre-Beta ({})'.format(PROJECT_URL)}
 
     def geocode(self, query):
         result = requests.get(self._search_endpoint,
-                params={
-                    'q': query, 'email': self._mail,
-                    'limit': 1, 'format': 'json'},
-                headers=self._headers).json()
+                              params={
+                                  'q': query, 'email': self._mail,
+                                  'limit': 1, 'format': 'json'},
+                              headers=self._headers
+                              ).json()
         if not result:
             raise ValueError('no geocode result found for {}'.format(query))
         return (float(result[0]['lat']), float(result[0]['lon']))
 
     def reverse_geocode(self, (lat, lon)):
         result = requests.get(self._reverse_endpoint,
-                params={
-                    'lat': lat, 'lon': lon, 'email': self._mail,
-                    'addressdetails': 0, 'format': 'json'},
-                headers=self._headers).json()
+                              params={
+                                  'lat': lat, 'lon': lon, 'email': self._mail,
+                                  'addressdetails': 0, 'format': 'json'},
+                              headers=self._headers).json()
         if result.get(u'error') == u'Unable to geocode':
             raise ValueError('cannot geocode {}'.format((lat, lon)))
         return result['display_name']
@@ -133,21 +138,21 @@ class Globe(object):
     '''
 
     def __init__(self, redis, geohash_precision,
-            cache_geocoding=20,
-            nominatim_search_endpoint=None,
-            nominatim_reverse_endpoint=None,
-            nominatim_mail=None,
-            namespace='',
-            data_serialize=json.dumps,
-            data_deserialize=json.loads):
+                 cache_geocoding=20,
+                 nominatim_search_endpoint=None,
+                 nominatim_reverse_endpoint=None,
+                 nominatim_mail=None,
+                 namespace='',
+                 data_serialize=json.dumps,
+                 data_deserialize=json.loads):
 
         self._redis = redis
         self._geohash_precision = geohash_precision
         self._cache_geocoding = cache_geocoding
         self._nominatim = NominatimGeocode(
-                mail=nominatim_mail,
-                search_endpoint=nominatim_search_endpoint,
-                reverse_endpoint=nominatim_reverse_endpoint)
+            mail=nominatim_mail,
+            search_endpoint=nominatim_search_endpoint,
+            reverse_endpoint=nominatim_reverse_endpoint)
         self._key_prefix = 'globe:{}:'.format(namespace)
         self._data_serialize = data_serialize
         self._data_deserialize = data_deserialize
@@ -159,7 +164,7 @@ class Globe(object):
             local pin_data = ARGV[4] -- which data should be saved for this pin
 
             -- get the current pin location
-            local pin_gh = redis.call('hget', key_prefix..'pins', pin_id) 
+            local pin_gh = redis.call('hget', key_prefix..'pins', pin_id)
 
             -- set pin data if requested
             if pin_data then
@@ -183,7 +188,7 @@ class Globe(object):
         self._delete_pin_script = redis.register_script('''
             local key_prefix = ARGV[1] -- prepend this to all keys
             local pin_id = ARGV[2]
-            
+
             local pin_gh = redis.call('hget', key_prefix..'pins', pin_id)
             if pin_gh then
                 redis.call('hdel', key_prefix..'pins', pin_id)
@@ -197,7 +202,8 @@ class Globe(object):
         gh = self._kws2geohash(kws)
         pin_data = kws.get('data')
         if pin_data:
-            args = [self._key_prefix, pin_id, gh, self._data_serialize(pin_data)]
+            args = [self._key_prefix, pin_id, gh,
+                    self._data_serialize(pin_data)]
         else:
             args = [self._key_prefix, pin_id, gh]
         self._add_or_move_pin_script(args=args)
@@ -207,7 +213,7 @@ class Globe(object):
 
     def near(self, size=1, **kws):
         gh = self._kws2geohash(kws)
-        geohashes = geohash_and_neighbours(gh, size)
+        geohashes = geohash_and_neighbors(gh, size)
         return self.make_area(geohashes)
 
     def almost_near(self, **kws):
@@ -233,15 +239,16 @@ class Globe(object):
             return ()
         pin_datas = self._redis.hmget(self._key_prefix + 'data', *pin_ids)
         return dict(zip(pin_ids,
-            ((self._data_deserialize(data) if data is not None else None)
-                    for data in pin_datas)))
+                        ((self._data_deserialize(data)
+                          if data is not None else None)
+                         for data in pin_datas)))
 
     def delete(self, pin_id):
         # TODO: can also delete a Area object and a list of pin ids
         success = self._delete_pin_script(args=[self._key_prefix, pin_id])
         if not success:
             raise ValueError('pin {} not found'.format(pin_id))
-    
+
     def __contains__(self, pin_id):
         return bool(self._redis.hexists(self._key_prefix + "pins", pin_id))
 
@@ -286,27 +293,29 @@ class Globe(object):
         return self._redis.hget(self._key_prefix + 'data', pin_id)
 
     def distance(self, **kws):
-        def calculate_distance(**other_kws):
-            from geopy.distance import great_circle
+        # def calculate_distance(**other_kws):
+        #     from geopy.distance import great_circle
         raise NotImplementedError()
-        #return calculate_distance
+        # return calculate_distance
 
     def _kws2geohash(self, kws):
 
         if 'latlon' in kws:
             return geohash.encode(kws['latlon'],
-                    precision=self._geohash_precision)
+                                  precision=self._geohash_precision)
 
         elif 'location' in kws:
 
             cache_geocoding = (
-                    kws['cache_geocoding'] if 'cache_geocoding' in kws
-                    else self._cache_geocoding)
+                kws['cache_geocoding'] if 'cache_geocoding' in kws
+                else self._cache_geocoding)
 
             # take geohash from cache if geocoding caching is enabled
             if cache_geocoding:
-                cache_key = hash_iter(('geocoding', 'google',
-                        str(self._geohash_precision), kws['location']))
+                cache_key = hash_iter(('geocoding',
+                                       'google',
+                                       str(self._geohash_precision),
+                                       kws['location']))
                 gh = self._redis.get(cache_key)
                 if gh:
                     return gh
@@ -314,7 +323,7 @@ class Globe(object):
             # geocode the location to a geohash
             lat, lon = self._nominatim.geocode(kws['location'])
             gh = geohash.encode(lat, lon,
-                    precision=self._geohash_precision)
+                                precision=self._geohash_precision)
 
             # if cache the geolocated location
             if cache_geocoding:
@@ -324,7 +333,7 @@ class Globe(object):
 
         elif 'geohash' in kws:
             return geohash.encode(*geohash.decode(kws['geohash']),
-                    precision=self._geohash_precision)
+                                  precision=self._geohash_precision)
 
         elif 'who' in kws:
             return self.geohash(kws['who'])
@@ -361,7 +370,8 @@ class Globe(object):
         used_labels = {}
         label_description = {}
         url = 'http://maps.googleapis.com/maps/api/staticmap?'
-        url += 'size={}x{}&maptype={}&sensor=false&scale=2'.format(size[0], size[1], maptype)
+        url += 'size={}x{}&maptype={}&sensor=false&scale=2'.format(
+            size[0], size[1], maptype)
         for cmd, gh, use_label, obj, in draw_geohashes:
                 if use_label:
                     # get the corresponding label
@@ -369,7 +379,7 @@ class Globe(object):
                     if not label:
                         try:
                             label = (ascii_labels.pop(0) if cmd == 'fill'
-                                    else number_labels.pop(0))
+                                     else number_labels.pop(0))
                         except IndexError:
                             # * will be showed as a cirlce in google maps
                             label = '*'
@@ -379,24 +389,28 @@ class Globe(object):
 
                     latlon = geohash.decode(gh)
                     url += '&markers=label:{label}|{lat},{lon}'.format(
-                            lat=round(latlon[0], 5), lon=round(latlon[1], 5), label=label)
+                        lat=round(latlon[0], 5),
+                        lon=round(latlon[1], 5),
+                        label=label)
                     url += '&path=fillcolor:red|weight:0|'
 
                 elif cmd == 'rect':
                     if use_label:
                         bbox = geohash.bbox(gh)
                         url += '&markers=label:{label}|color:blue|{lat},{lon}'.format(
-                                lat=round(bbox['n'], 5), lon=round(bbox['e'], 5), label=label)
+                            lat=round(bbox['n'], 5),
+                            lon=round(bbox['e'], 5),
+                            label=label)
                     url += '&path='
                 else:
                     assert False
 
                 bbox = geohash.bbox(gh)
                 points = [(bbox['n'], bbox['w']),
-                        (bbox['n'], bbox['e']),
-                        (bbox['s'], bbox['e']),
-                        (bbox['s'], bbox['w']),
-                        (bbox['n'], bbox['w'])]
+                          (bbox['n'], bbox['e']),
+                          (bbox['s'], bbox['e']),
+                          (bbox['s'], bbox['w']),
+                          (bbox['n'], bbox['w'])]
                 p = polyenc.encode([(p[1], p[0]) for p in points])
                 polyline_encoded = p['points']
                 url += 'enc:{}'.format(polyline_encoded)
@@ -409,7 +423,7 @@ class Globe(object):
             return label_description, url
         else:
             # maybe the browser autocodes charachters so actually more are used
-            print 'image: {} ({} chars, 2048 allowed)'.format(url, len(url)) 
+            print 'image: {} ({} chars, 2048 allowed)'.format(url, len(url))
             print
             print 'image legend'
             print '------------'
@@ -431,8 +445,8 @@ class Area(object):
 
     def __iter__(self):
         pin_ids = self._redis.sunion(
-                *(self._key_prefix + "gh:" + gh for gh in self.geohashes))
-        return iter(sorted(pin_ids)) # sorted to make the results more consistent
+            *(self._key_prefix + "gh:" + gh for gh in self.geohashes))
+        return iter(sorted(pin_ids))  # sorted makes the results more consistent
 
     def __len__(self):
         pipe = self._redis.pipeline()
@@ -450,15 +464,15 @@ class Area(object):
         if not isinstance(other, Area):
             raise TypeError('other must also be a Area')
         return Area(self._redis,
-                self.geohashes.intersection(other.geohashes),
-                key_prefix=self._key_prefix)
+                    self.geohashes.intersection(other.geohashes),
+                    key_prefix=self._key_prefix)
 
     def __or__(self, other):
         if not isinstance(other, Area):
             raise TypeError('other must also be a Area')
         return Area(self._redis,
-                self.geohashes.union(other.geohashes),
-                key_prefix=self._key_prefix())
+                    self.geohashes.union(other.geohashes),
+                    key_prefix=self._key_prefix())
 
     def __eq__(self, other):
         if not isinstance(other, Area):
@@ -472,7 +486,7 @@ class Area(object):
         else:
             more = ''
         return '<Area containing {} pins{}, size {} >'.format(
-                len(self), more, len(self.geohashes))
+            len(self), more, len(self.geohashes))
 
     @property
     def geohashes(self):
